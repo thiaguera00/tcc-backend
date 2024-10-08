7ad247bc97db1253a44acb5d1845dc57cd0338e5
thiaguera00 <thiagocaetano155@gmail.com>
2024-10-08 16:54:58 -0300

    feat: added CORS

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,27 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('API TCC')
-    .setDescription('documentação da API TCC')
+    .setDescription('Documentação da API TCC')
     .setVersion('1.0')
     .addTag('Rotas')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
+
 bootstrap();
